e949f4340659dfa879a7ae57bcacc70c06c24337
HJeff <JHHsieh1990@gmail.com>
2019-09-27 01:28:14 -0700

    Window for translator
    Maybe. This is a test and I may put it into the body of the other Main

--- Main.java
+++ Main.java
@@ -1,75 +1,37 @@
 package com.company;
 
-import java.util.HashMap;
-import java.util.Map;
 import javax.swing.*;
 import java.awt.*;
-import javax.swing.JFrame;
-
-
-//hashmap for run to english data
-
 
 public class Main {
 
-    public static hashmap(){
-        HashMap<String, String> engToRunes = new HashMap<String, String>;
-
-        engToRunes.put("A", "ᚨ");
-        engToRunes.put("B", "ᛒ");
-        engToRunes.put("C", "ᚲ");
-        engToRunes.put("D", "ᛞ");
-        engToRunes.put("E", "ᛖ");
-        engToRunes.put("F", "ᚠ");
-        engToRunes.put("G", "ᚷ");
-        engToRunes.put("H", "ᚺ");
-        engToRunes.put("I", "ᛁ");
-        engToRunes.put("J", "ᛃ");
-        engToRunes.put("K", "ᚴ");
-        engToRunes.put("L", "ᛚ");
-        engToRunes.put("M", "ᛗ");
-        engToRunes.put("N", "ᚾ");
-        engToRunes.put("O", "ᛟ");
-        engToRunes.put("P", "ᛈ");
-        engToRunes.put("Q", "ᛩ");
-        engToRunes.put("R", "ᚱ");
-        engToRunes.put("S", "ᛋ");
-        engToRunes.put("T", "ᛏ");
-        engToRunes.put("U", "ᚢ");
-        engToRunes.put("V", "ᚡ");
-        engToRunes.put("W", "ᚹ");
-        engToRunes.put("X", "ᛪ");
-        engToRunes.put("Y", "ᚤ");
-        engToRunes.put("Z", "ᛉ");
-
-
-
-
-    }
-
-
-
-    public void EnglishToRune(){
-
-        JFrame frame = new JFrame("English to rune translator");
-        frame.setVisible(true);
-        frame.setSize(900, 800);
-
-        JLabel englishLabel = new JLabel("English here");
-        englishLabel.setHorizontalAlignment((SwingConstants.CENTER);
-        JButton englishToRune = new JButton("Translate!");
-
-
-
-    }
-
-
-
-
-
+    //JFrame is a container. Holds button lists, etc.
 
 
     public static void main(String[] args) {
-	// write your code here
+        JFrame frame = new JFrame();
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.BLACK);
+        frame.getContentPane().add(panel);
+
+        JButton button = new JButton("Button");
+        panel.add(button);
+
+        JTextField textField = new JTextField();
+        textField.setPreferredSize(new Dimension(200, 15));
+        panel.add(textField);
+
+        JButton button2 = new JButton ("Second button.");
+        panel.add(button2);
+
+        frame.setTitle("LearningJFrame"); //title
+        frame.setSize(500,400); //setting window dimension
+        frame.setLocationRelativeTo(null); //start location. Null sets to middle of screen
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //default close action
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+
     }
 }
